e228175abb13c02420ec29e9b8a7c702b98f7b68
HillTrigger <mslava32@gmail.com>
2024-10-10 18:13:05 +0600

    Completed header layout

---
+++ components/header/header.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import logoSvg from "./logo.svg";
+import avatar from "./avatar.jpeg";
+
+export default function Header() {
+  return (
+    <div className="flex items-center bg-white px-8 py-6">
+      <Image src={logoSvg} alt="logo" />
+      <div className="w-px bg-slate-200 h-7 mx-6" />
+      <button className="text-2xl text-white bg-teal-600 transition-colors rounded-lg leading-tight px-12 py-2 hover:bg-teal-500">
+        Играть
+      </button>
+      <button className="flex gap-2 ml-auto items-center text-teal-600 hover:text-teal-500">
+        <Image
+          src={avatar}
+          alt="avatar"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+        <div className="text-left leading-tight">
+          <div className="text-lg">Paromovevg</div>
+          <div className="text-slate-400 text-xs">Рейтинг: 1230</div>
+        </div>
+        <svg
+          width="13"
+          height="7"
+          viewBox="0 0 13 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6.18753 6.75003C6.04368 6.75003 5.89968 6.69505 5.78985 6.58522L0.164848 0.960222C-0.0549492 0.740426 -0.0549492 0.384504 0.164848 0.164848C0.384645 -0.0548086 0.740566 -0.0549492 0.960222 0.164848L6.18753 5.39216L11.4148 0.164848C11.6346 -0.0549492 11.9906 -0.0549492 12.2102 0.164848C12.4299 0.384645 12.43 0.740566 12.2102 0.960222L6.58522 6.58522C6.47539 6.69505 6.33139 6.75003 6.18753 6.75003Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
